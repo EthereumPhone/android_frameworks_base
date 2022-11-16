59eb4d4c34472af5386da974281a7840ef9b723a
Markus Haas <haas.markus@protonmail.com>
2022-11-16 15:08:49 +0100

    Update light node service
    Change-Id: I15081003f17e542acd4916e8950011097b4df336

--- services/java/com/android/server/GethService.java
+++ services/java/com/android/server/GethService.java
@@ -12,17 +12,34 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
-
 public class GethService extends IGethService.Stub {
     private static final String TAG = "GethService";
     private static GethService instance;
     private Node node;
     private String dataDir;
-    
+    private Runnable startRunnable;
+    private Runnable stopRunnable;
+
+    static {
+        System.loadLibrary("helios");
+    }
+
     public GethService() {
         super();
         dataDir = Environment.getDataDirectory().getAbsolutePath();
         Log.v(TAG, "GethNode, onCreate" + dataDir);
+
+        startRunnable = new Runnable(){
+            public void run(){
+                startClient();
+            }
+        };
+
+        stopRunnable = new Runnable(){
+            public void run(){
+                stopClient();
+            }
+        };
 
         // If file doesnt exist, create it
         if(!doesFileExist(dataDir+"/currentStatus.txt")) {
@@ -40,15 +57,7 @@ public class GethService extends IGethService.Stub {
         if (wantToStart(dataDir+"/currentStatus.txt")) {
             // Setting Verbosity to 4 to see what is happening
             try {
-                Geth.setVerbosity(4);
-                NodeConfig nodeConfig = Geth.newNodeConfig();
-                node = Geth.newNode(dataDir+"/.ethNode", nodeConfig);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                node.start();
-                
+                new Thread(startRunnable).start();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -61,43 +70,30 @@ public class GethService extends IGethService.Stub {
     }
 
     public String getEnodeURL() {
-        return node.getNodeInfo().getEnode();
+        return "";
     }
 
     public void shutdownGeth() {
-        try {
-            node.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        savePreference(dataDir+"/currentStatus.txt", false);
+        savePreference(dataDir + "/currentStatus.txt", false);
+        new Thread(stopRunnable).start();
         Log.v(TAG, "GethNode, successfully stopped :)");
     }
 
     public void shutdownWithoutPreference() {
-        try {
-            node.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new Thread(stopRunnable).start();
         Log.v(TAG, "GethNode, successfully stopped. Without preferences :)");
     }
-    
+
+    public native void startClient();
+    public native void stopClient();
+
     public void startGeth() {
         try {
-            Geth.setVerbosity(4);
-            NodeConfig nodeConfig = Geth.newNodeConfig();
-            node = Geth.newNode(Environment.getDataDirectory()+"/.ethNode", nodeConfig);
+            new Thread(startRunnable).start();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        try {
-            node.start();
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        savePreference(dataDir+"/currentStatus.txt", true);
+        savePreference(dataDir + "/currentStatus.txt", true);
         Log.v(TAG, "GethNode, successfully started :)");
     }
 
@@ -107,23 +103,25 @@ public class GethService extends IGethService.Stub {
     }
 
     public boolean wantToStart(String filename) {
-        if(!doesFileExist(filename)) {
+        if (!doesFileExist(filename)) {
             try {
                 File file = new File(filename);
                 file.createNewFile();
                 FileWriter myWriter = new FileWriter(filename);
                 myWriter.write("true");
                 myWriter.close();
-            } catch(IOException exception) {
+            } catch (IOException exception) {
                 exception.printStackTrace();
             }
-            
+
             return true;
         }
         String content = "true";
         try {
-            content = new Scanner(new File(filename)).useDelimiter("\\Z").next();
-        } catch(FileNotFoundException exception) {
+            Scanner scanner = new Scanner(new File(filename));
+            content = scanner.useDelimiter("\\Z").next();
+            scanner.close();
+        } catch (FileNotFoundException exception) {
             exception.printStackTrace();
         }
 
@@ -145,10 +143,10 @@ public class GethService extends IGethService.Stub {
                 myWriter.write("false");
             }
             myWriter.close();
-        } catch(IOException exception) {
+        } catch (IOException exception) {
             exception.printStackTrace();
         }
-        
+
     }
-    
+
 }
